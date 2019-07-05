365c7961c2f5d723afc34f3eeed444a8b9b6f4eb
demenev <demenev@skbkontur.ru>
2019-07-05 17:17:57 +0500

    fill project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        lowLevelMethod();
+    }
+
+    private static void lowLevelMethod() {
+        int result = 1/0;
+    }
+}
